83c5280c063045baaa908539b9521a92f6c1cf05
TheLooped <95268961+TheLooped@users.noreply.github.com>
2023-12-24 19:16:39 -0500

    Updated keymaps

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -14,7 +14,6 @@ local function map(modes, lhs, rhs, opts)
 
 	for _, mode in ipairs(modes) do
 		if type(rhs) == "function" then
-			-- Convert the function to a string representation
 			local fn_str = string.format("<cmd>lua %s()<cr>", tostring(rhs))
 			vim.api.nvim_set_keymap(mode, lhs, fn_str, options)
 		else
@@ -35,7 +34,7 @@ local function newFile()
 		local fname = vim.fn.input("FileName:")
 
 		if fname == "" then
-			print("No file name")
+			vim.notify("No file name")
 			return
 		else
 			vim.cmd([[enew]])
@@ -56,16 +55,16 @@ local function saveFile()
 
 				if new_filename and new_filename ~= "" then
 					vim.cmd('exec "w " .. fnameescape("' .. new_filename .. '")')
-					print("File saved successfully")
+					vim.notify("File saved successfully")
 				else
-					print("Save aborted: Empty filename")
+					vim.notify("Save aborted: Empty filename")
 				end
 			else
-				print("Save aborted: No file name provided")
+				vim.notify("Save aborted: No file name provided")
 			end
 		else
 			vim.cmd("w")
-			print("File saved successfully")
+			vim.notify("File saved successfully")
 		end
 	end
 end
@@ -74,18 +73,34 @@ local function makeExe()
 	return function()
 		local current_file = vim.fn.expand("%:t")
 
+		-- Check if the file is already executable
+		if vim.fn.executable(current_file) == 1 then
+			vim.notify("File is already executable")
+			return
+		end
+
 		-- Check if the file has a Bash extension
 		if vim.fn.match(current_file, ".sh$") > 0 then
-			local cmd = { "chmod", "+x", current_file }
-			local result = require("utils.helper").cmd(cmd)
+			local user_input = vim.fn.input("Do you want to make the file executable? (y/n): ")
+			if user_input:lower() == "y" then
+				local cmd = { "chmod", "+x", current_file }
+				local result = require("utils.helper").cmd(cmd)
 
-			if result then
-				print("File is now executable")
+				if result then
+					vim.notify("File is now executable")
+				else
+					vim.notify("Failed to make file executable")
+				end
 			else
-				print("Failed to make file executable")
+				vim.notify("No changes made.")
 			end
 		else
-			print("This is not a Bash script. No changes made.")
+			local user_input = vim.fn.input("This is not a Bash script. Do you still want to proceed? (y/n): ")
+			if user_input:lower() == "y" then
+				vim.notify("No changes made.")
+			else
+				vim.notify("Aborted.")
+			end
 		end
 	end
 end
@@ -155,7 +170,7 @@ map("n", "<leader>q", function()
 	require("utils.helper").confirm_quit()
 end, { desc = "Quit" })
 
--- Misc
+-- Quality of life
 map("n", "<leader>h", "<cmd>nohlsearch<cr>", { desc = "Clear search highlights" })
 map("i", "<leader>j", "<Esc>", { desc = "Exit Insert" })
 map("n", "J", "mzJ`z", { desc = "Better line joining" })
@@ -175,24 +190,69 @@ map("n", "<A-k>", ":resize +2<CR>", { desc = "Increase split height" })
 
 --### Plugins
 
+-- Plugin Manager
+map("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
 --- Git Related
+
 --- GitSigns
-
+map("n", "g]", "<cmd>Gitsigns next_hunk<cr>", { desc = "Next Git hunk" })
+map("n", "g[", "<cmd>Gitsigns prev_hunk<cr>", { desc = "Previous Git hunk" })
+map("n", "<leader>gl", "<cmd>Gitsigns blame_line<cr>", { desc = "View Git blame" })
+map("n", "<leader>gL", "<cmd>Gitsigns blame_line { full = true }<cr>", { desc = "View full Git blame" })
+map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { desc = "Preview Git hunk" })
+map("n", "<leader>gh", "<cmd>Gitsigns reset_hunk<cr>", { desc = "Reset Git hunk" })
+map("n", "<leader>gr", "<cmd>Gitsigns reset_buffer<cr>", { desc = "Reset Git buffer" })
+map("n", "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", { desc = "Stage Git hunk" })
+map("n", "<leader>gS", "<cmd>Gitsigns stage_buffer<cr>", { desc = "Stage Git buffer" })
+map("n", "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", { desc = "Unstage Git hunk" })
+map("n", "<leader>gd", "<cmd>Gitsigns diffthis<cr>", { desc = "View Git diff" })
 --- LSP Related
--- LspUI
--- Conform
--- Codeium
 
---- Treesitter Related
+-- LspUI
+map("n", "<leader>lh", "<cmd>LspUI hover<cr>", { desc = "LspUI hover" })
+map("n", "<leader>ln", "<cmd>LspUI rename<cr>", { desc = "LspUI Rename" })
+map("n", "<leader>ld", "<cmd>LspUI diagnostic<cr>", { desc = "LspUI Diagnostic" })
+map("n", "<leader>la", "<cmd>LspUI code_action<cr>", { desc = "LspUI Code_Action" })
+map("n", "<leader>lr", "<cmd>LspUI reference<cr>", { desc = "LspUI Reference" })
+--map("n", "<leader>li", "<cmd>LspUI<cr>", { desc = "LspUI" })
+map("n", "<leader>li", "<cmd>LspUI implementation<cr>", { desc = "LspUI Implementation" })
+map("n", "<leader>lt", "<cmd>LspUI type_definition<cr>", { desc = "LspUI Type Definition" })
+map("n", "<leader>lp", "<cmd>LspUI declaration<cr>", { desc = "LspUI Declaration" })
+map("n", "<leader>lf", "<cmd>LspUI definition<cr>", { desc = "LspUI Definition" })
 
 --- Telescope Related
 
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find Files Global" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Fuzzy Find in Buffer" })
 -- Editor Related
--- Comment.nvim
--- Surrond.nvim
--- Pairs.nvim
+
 -- Search And Replace
+map("n", "<leader>mt", "<cmd>MurenToggle<cr>", { desc = "Toggles Muren" })
+map("n", "<leader>mu", "<cmd>MurenToggle<cr>", { desc = "Unique Muren Instance" })
+map("n", "<leader>mf", "<cmd>MurenFresh<cr>", { desc = "Fresh New Muren Instance" })
+
 -- Mini files
-map("n", "<leader>n", "<cmd>lua MiniFiles.open()<cr>", { desc = "Open Mini Files" })
+map("n", "<leader>fe", "<cmd>lua MiniFiles.open()<cr>", { desc = "Open Mini Files" })
+
+-- Grapple Related
+map("n", "<leader>gt", "<cmd>GrappleToggle<cr>", { desc = "Grapple Toggle tag" })
+map("n", "<leader>gpt", "<cmd>GrapplePopup tags<cr>", { desc = "Grapple Popup tags" })
+map("n", "<leader>gps", "<cmd>GrapplePopup scopes<cr>", { desc = "Grapple Popup scope" })
+map("n", "<leader>t[", "<cmd>GrappleCycle backward<cr>", { desc = "Grapple cycle back" })
+map("n", "<leader>t]", "<cmd>GrappleCycle forward<cr>", { desc = "Grapple " })
+
+map("n", "<leader>gs", function()
+	local name = vim.fn.input({ prompt = "Tag Name: " })
+	require("grapple").select({ key = name })
+end, { desc = "Grapple Select" })
+
+map("n", "<leader>gT", function()
+	local name = vim.fn.input({ prompt = "Tag Name: " })
+	require("grapple").tag({ key = name })
+end, { desc = "Grapple Tag with key" })
+-- Themery
+map("n", "<leader>th", "<cmd>Themery<cr>", { desc = "Themery" })
 
 --### Toggles
